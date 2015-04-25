eba34c96602b8cea005c3572f4f8be09ac3e6f93
realduke2000 <hou_hl@sina.com>
2015-04-25 10:43:39 +0800

    updated

--- myd3.js
+++ myd3.js
@@ -1,8 +1,16 @@
-function draw() {
-    d3.selectAll("p")
-      .data(data)
-      .enter()
-            .text(function (d) {
-                return d;
-            });
+function test() {
+  alert("called test");
+}
+
+function plus() {
+
+var i = 1;
+  d3.selectAll("circle")
+  .attr("fill", "fill:#" + i);
+  /*for (var i = 0; i < 1000000; i++)
+  {
+    d3.selectAll("circle")
+      .attr("fill","fill:#"+i)
+  }
+  */
 }
